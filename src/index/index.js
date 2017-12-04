96f71fcda8bfb6f9d2bd041440cbb6aadfdedd81
daping <1478642895@qq.com>
2017-12-04 12:26:03 +0800

    分页

--- src/index/index.js
+++ src/index/index.js
@@ -3,7 +3,7 @@ function reset() {
 	let $content = $('#content.post-index'); //父级
 	let $articles = $content.find('article'); //卡片集合
 	let content_width = $content.width(); //父级宽度
-	let articles_width_old =249 + _margin; //卡片初始化
+	let articles_width_old = 249 + _margin; //卡片初始化
 
 	//一行能显示的卡片数量
 	let max_column = parseInt(content_width / articles_width_old);
@@ -42,4 +42,50 @@ reset();
 window.onresize = function() {
 	reset();
 }
+qingqqiu();
+//分页
+function qingqqiu() {
+	let total = $('#total').val();
+	let yesu = (total / 9)+1;
+	console.log("会执行吗" + yesu)
+	if(yesu > 1) {}
+	$('#shangye').on('click', function(e) {
+		let ddd=getUrlParam('page')
+		if(!ddd){
+			qq(1);
+			return;
+		}
+		let dd=--ddd;
+		if(dd==0){
+			return;
+		}
+		qq(dd);
+		
+	})
+	$('#xiaye').on('click', function(e) {
+		let ddd=getUrlParam('page');
+		if(ddd==null){
+			qq(2);
+			return;
+		}
+		let dd=++ddd;
+		if(dd>yesu){
+			$.Prompt("欢迎光临本站!4S",4000);("已经是最后一页了");
+			return;
+		}
+		qq(dd);
+	})
 
+}
+
+function qq(rint) {
+	window.location.href = '/index?page=' + rint;
+}
+
+//获取url中的参数
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	var r = window.location.search.substr(1).match(reg); //匹配目标参数
+	if(r != null) return unescape(r[2]);
+	return null; //返回参数值
+}
